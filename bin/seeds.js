08531c3f630d844566eb812b72b35188d8b2433e
iourisorokine <isorokine@gmail.com>
2019-10-04 10:36:46 +0200

    add seed gems for test

--- bin/seeds.js
+++ bin/seeds.js
@@ -29,6 +29,97 @@ let users = [
   }
 ]
 
+const gems=[
+  // example 1
+  {
+    title: "Niagara Waterfall",
+    description: "we captured this amazing picture of the world famous marvel",
+    good_to_know: "water is cold",
+
+    image_url: "https://photographycourse.net/wp-content/uploads/2014/11/Landscape-Photography-steps.jpg",
+    creator: "",
+    discovery: true,
+    category:"Nature",
+    visitedDate: "",
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
+  },
+  // example 2
+  {
+    title: "This super spot",
+    description: "That's a very cool spot, we just ca't believe that",
+    good_to_know: "Spot spot spot",
+
+    image_url: "https://photographycourse.net/wp-content/uploads/2014/11/Landscape-Photography-steps.jpg",
+    creator: "",
+    discovery: true,
+    category:"Nature",
+    visitedDate: "",
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
+  },
+
+    // example 3
+    {
+      title: "Hiking in the Mountains",
+      description: "That's a very cool spot, we just ca't believe that",
+      good_to_know: "for advanced hickers only",
+  
+      image_url: "https://photographycourse.net/wp-content/uploads/2014/11/Landscape-Photography-steps.jpg",
+      creator: "",
+      discovery: true,
+      category:"Hikes",
+      visitedDate: "",
+      timestamps: {
+        createdAt: "created_at",
+        updatedAt: "updated_at"
+      }
+    },
+
+        // example 4
+        {
+          title: "Hiking in the Mountains",
+          description: "That's a very cool spot, we just ca't believe that",
+          good_to_know: "for advanced hickers only",
+      
+          image_url: "https://photographycourse.net/wp-content/uploads/2014/11/Landscape-Photography-steps.jpg",
+          creator: "",
+          discovery: true,
+          category:"Hikes",
+          visitedDate: "",
+          timestamps: {
+            createdAt: "created_at",
+            updatedAt: "updated_at"
+          }
+        },
+
+            // example 5
+    {
+      title: "The History museum",
+      description: "Discover the history and cultureof locals and ravel trough time",
+      good_to_know: "20usd admission fee",
+  
+      image_url: "https://photographycourse.net/wp-content/uploads/2014/11/Landscape-Photography-steps.jpg",
+      creator: "",
+      discovery: true,
+      category:"cultureArt",
+      visitedDate: "",
+      timestamps: {
+        createdAt: "created_at",
+        updatedAt: "updated_at"
+      }
+    }
+
+]
+
+
+
+
+
 User.deleteMany()
 .then(() => {
   return User.create(users)
