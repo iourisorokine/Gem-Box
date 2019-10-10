394f447cd8f66516240594d16817dc81bb47d758
Jrgsto <stommel.joerg@gmail.com>
2019-10-10 17:07:41 +0200

    Updated CreatedGem Process

--- client/src/components/create/AddExperience.js
+++ client/src/components/create/AddExperience.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
 import axios from "axios";
 import { Formik } from "formik";
+import "../../stylesheets/createGem.css";
 
 import {
   Image,
@@ -72,89 +73,104 @@ export default class AddExperience extends React.Component {
   render() {
     return (
       <div>
-        <h1> Tell others about your Experience</h1>
-        {this.props.locationName && (
-          <h2>{this.props.locationName.substring(0, 30)}...</h2>
-        )}
-
-        {this.state.message && (
-          <div>
-            <h3>{this.state.message}</h3>
-            <button onClick={this.noPicProceed} name="yes">
-              Yes
-            </button>
-            <button onClick={this.noPicProceed} name="no">
-              No
-            </button>
-          </div>
-        )}
-        <Form onSubmit={this.handleSubmit}>
-          <Form.Group>
-            <Form.Label htmlFor="title">Title: </Form.Label>
-            <Form.Control
-              required
-              onChange={this.handleChanges}
-              type="text"
-              name="title"
-              id="title"
-              value={this.props.title}
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label htmlFor="description">Description: </Form.Label>
-            <Form.Control
-              required
-              onChange={this.handleChanges}
-              type="text"
-              name="description"
-              id="description"
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label htmlFor="category">Category </Form.Label>
-            <Form.Control
-              required
-              onChange={this.handleChanges}
-              name="category"
-              as="select"
+        <div className="pageheader">
+          <h4> Tell others about your Experience</h4>
+        </div>
+        <div className="padding-wrapper">
+          {this.props.locationName && (
+            <h2>{this.props.locationName.substring(0, 30)}...</h2>
+          )}
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Group>
+              <Form.Label htmlFor="title">Title: </Form.Label>
+              <Form.Control
+                required
+                onChange={this.handleChanges}
+                type="text"
+                name="title"
+                id="title"
+                value={this.props.title}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label htmlFor="description">Description: </Form.Label>
+              <Form.Control
+                required
+                onChange={this.handleChanges}
+                type="text"
+                name="description"
+                id="description"
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label htmlFor="category">Category </Form.Label>
+              <Form.Control
+                required
+                onChange={this.handleChanges}
+                name="category"
+                as="select"
+              >
+                <option value="">Choose a category</option>
+                <option value="cultureArts">Culter & Arts</option>
+                <option value="foodDrinks">Food & Drinks</option>
+                <option value="hikes">Hikes</option>
+                <option value="nature">Nature</option>
+                <option value="party">Party</option>
+                <option value="sports">Sports</option>
+                <option value="others">Others</option>
+              </Form.Control>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label htmlFor="goodToKnow">Good to know: </Form.Label>
+              <Form.Control
+                required
+                onChange={this.handleChanges}
+                type="text"
+                name="goodToKnow"
+                id="goodToKnow"
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label htmlFor="visitedDate">Date: </Form.Label>
+              <Form.Control
+                required
+                onChange={this.handleChanges}
+                type="date"
+                name="visitedDate"
+                id="visitedDate"
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Control type="file" name="imageUrl" />
+            </Form.Group>
+            {this.state.message && (
+              <div className="dangerMessage">
+                <p>{this.state.message}</p>
+                <button
+                  className="btn btn-primary generalBtn"
+                  onClick={this.noPicProceed}
+                  name="yes"
+                >
+                  Yes
+                </button>
+                <button
+                  className="btn btn-primary generalBtn"
+                  onClick={this.noPicProceed}
+                  name="no"
+                >
+                  No
+                </button>
+              </div>
+            )}
+            <Button
+              className="btn btn-primary generalBtn btnexperience"
+              disabled={!this.state.isEnabled}
+              type="submit"
             >
-              <option value="">Choose a category</option>
-              <option value="cultureArts">Culter & Arts</option>
-              <option value="foodDrinks">Food & Drinks</option>
-              <option value="hikes">Hikes</option>
-              <option value="nature">Nature</option>
-              <option value="party">Party</option>
-              <option value="sports">Sports</option>
-              <option value="others">Others</option>
-            </Form.Control>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label htmlFor="goodToKnow">Good to know: </Form.Label>
-            <Form.Control
-              required
-              onChange={this.handleChanges}
-              type="text"
-              name="goodToKnow"
-              id="goodToKnow"
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label htmlFor="visitedDate">Date: </Form.Label>
-            <Form.Control
-              required
-              onChange={this.handleChanges}
-              type="date"
-              name="visitedDate"
-              id="visitedDate"
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Control type="file" name="imageUrl" />
-          </Form.Group>
-          <Button disabled={!this.state.isEnabled} type="submit">
-            Create Gem
-          </Button>
-        </Form>
+              Create Experience >
+            </Button>
+          </Form>
+        </div>
       </div>
     );
   }
