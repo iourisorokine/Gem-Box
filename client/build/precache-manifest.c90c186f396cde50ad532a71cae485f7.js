98c1b3a59e6e706856f782ba6b03bb9307b9dd72
iourisorokine <isorokine@gmail.com>
2019-10-10 15:09:01 +0200

    deploy nb2 with build

---
+++ client/build/precache-manifest.c90c186f396cde50ad532a71cae485f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eecb24240c338ae298734831bcfd966",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c688c0d6d58380fcc7ce",
+    "url": "/static/css/2.4a0441e8.chunk.css"
+  },
+  {
+    "revision": "375ff15c21bfc0f5ea90",
+    "url": "/static/css/main.90632149.chunk.css"
+  },
+  {
+    "revision": "c688c0d6d58380fcc7ce",
+    "url": "/static/js/2.6faf1f2c.chunk.js"
+  },
+  {
+    "revision": "375ff15c21bfc0f5ea90",
+    "url": "/static/js/main.a47ef4e6.chunk.js"
+  },
+  {
+    "revision": "f1c6019deaec6f4a9d0b",
+    "url": "/static/js/runtime-main.11bc70d3.js"
+  },
+  {
+    "revision": "aaa5678430349172ebef931252521450",
+    "url": "/static/media/jeremy-bishop-dvACrXUExLs-unsplash_cut.aaa56784.png"
+  }
+]);
