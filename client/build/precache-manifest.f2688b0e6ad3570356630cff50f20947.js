4c628745f7f2e89b621dd0f5d6db4b53decb969f
iourisorokine <isorokine@gmail.com>
2019-10-16 11:50:04 +0200

    build ready for deployment

---
+++ client/build/precache-manifest.f2688b0e6ad3570356630cff50f20947.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e249d57a345e4c673933d855bf3e9561",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9df1bc9e685ecbca08f",
+    "url": "/static/css/2.4a0441e8.chunk.css"
+  },
+  {
+    "revision": "32e99d98545b1ee24329",
+    "url": "/static/css/main.ec8b950e.chunk.css"
+  },
+  {
+    "revision": "a9df1bc9e685ecbca08f",
+    "url": "/static/js/2.9f73064f.chunk.js"
+  },
+  {
+    "revision": "32e99d98545b1ee24329",
+    "url": "/static/js/main.9a651481.chunk.js"
+  },
+  {
+    "revision": "f1c6019deaec6f4a9d0b",
+    "url": "/static/js/runtime-main.11bc70d3.js"
+  },
+  {
+    "revision": "aaa5678430349172ebef931252521450",
+    "url": "/static/media/jeremy-bishop-dvACrXUExLs-unsplash_cut.aaa56784.png"
+  }
+]);
